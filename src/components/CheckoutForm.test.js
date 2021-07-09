edda56a01f30a41ab8c11a00be96ff98aabfa5d7
sean-mahan <seanmahansean@gmail.com>
2021-07-09 13:37:11 -0500

    push try

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,9 +1,34 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen} from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import userEvent from "@testing-library/user-event";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+  render(<CheckoutForm/>);
 
-test("form shows success message on submit with form details", () => {});
+  const header = screen.queryByText(/Checkout Form/i);
+
+  expect(header).toBeInTheDocument();
+});
+
+test("form shows success message on submit with form details", () => {
+  render(<CheckoutForm/>);
+
+  const fname = screen.getByLabelText(/first name/i);
+  const lname = screen.getByLabelText(/last name/i);
+  const addy = screen.getByLabelText(/address/i);
+  const city = screen.getByLabelText(/city/i);
+  const state = screen.getByLabelText(/state/i);
+  const zip = screen.getByLabelText(/zip/i);
+  const btn = screen.getByLabelText(/button/i);
+
+  userEvent.type(fname, "text");
+  userEvent.type(lname, "text");
+  userEvent.type(addy, "text");
+  userEvent.type(city, "text");
+  userEvent.type(state, "text");
+  userEvent.type(zip, "text");
+  userEvent.type(btn);
+});
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,14 @@
 // write your custom hook here to control your checkout form
+import {useState} from "react";
+
+const useForm = (initVals) => {
+  const [vals, setVals] = useState(initVals);
+
+  const handleChanges = e => {
+    setVals({...vals, [e.target.name]:e.target.value})
+  }
+
+  return [vals, handleChanges];
+}
+
+export default useForm;
